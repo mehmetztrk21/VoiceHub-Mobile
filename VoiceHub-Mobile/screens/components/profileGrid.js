f064cab4996e24433ed606d6bd6c020b760298be
Kaan Kayserili <knkayserili@gmail.com>
2023-02-03 14:03:02 +0300

    profileGrid.js style exported editing

--- VoiceHub-Mobile/screens/components/profileGrid.js
+++ VoiceHub-Mobile/screens/components/profileGrid.js
@@ -3,7 +3,7 @@ import {TouchableOpacity,} from "react-native";
 import profileGridStyle from "../../assets/styles/profileGrid.style";
 export default function PostImg({ postImg }) {
   return (
-    <TouchableOpacity style={styles.post}>
+    <TouchableOpacity style={profileGridStyle.post}>
       {/* Voices is here */}
     </TouchableOpacity>
   );
