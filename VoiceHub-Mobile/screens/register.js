970fcf118c0d864d5db5730d65eee528efc65997
Kaan Kayserili <knkayserili@gmail.com>
2023-02-06 00:05:54 +0300

    create register page, but not congirute design

---
+++ VoiceHub-Mobile/screens/register.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { TouchableOpacity, View, TextInput, Text } from "react-native";
+import registerStyle from "../../assets/styles/register.style";
+
+export default function Register() {
+    return (
+        <View style={loginStyle.container}>
+
+            <Text>Name</Text>
+            <TextInput />
+
+            <Text>Last Name</Text>
+            <TextInput />
+
+            <Text>E-Mail</Text>
+            <TextInput keyboardType="email-address"/>
+
+            <Text>User Name</Text>
+            <TextInput />
+
+            <Text>Password</Text>
+            <TextInput keyboardType="visible-password"/>
+
+            <Text>Password Repeat</Text>
+            <TextInput keyboardType="visible-password"/>
+
+            <TouchableOpacity>
+                <Text>Register</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
