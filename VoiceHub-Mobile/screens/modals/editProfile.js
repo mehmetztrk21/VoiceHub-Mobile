9ae8ed78c99fd3892307a8db8633cdde1f9997ae
Kaan Kayserili <knkayserili@gmail.com>
2023-02-12 02:31:50 +0300

    edit profile page designed but continue

--- VoiceHub-Mobile/screens/modals/editProfile.js
+++ VoiceHub-Mobile/screens/modals/editProfile.js
@@ -1,38 +1,58 @@
 import React from 'react';
-import { View, Text, Image, Modal } from 'react-native';
+import { View, Text, Image, Modal, TextInput, TouchableOpacity } from 'react-native';
+import { Icon } from "react-native-elements"
 import editProfileStyle from "../../assets/styles/editProfile.style"
 import Slider from "../components/slider";
 import user1 from "../../assets/userImages/user1.jpg";
+
 export default function EditProfile(editVisible) {
-  const hasBio=true;
-    return (  
-      <Modal style={{width:"100%"}} onRequestClose={()=>{!editVisible}}>
-    <View>
-    {editVisible.editVisible ?  (
-      <View style={editProfileStyle.container}>
-      <View style={editProfileStyle.top}>
-        <Text style={editProfileStyle.header}>Edit Profile</Text>
-      </View>
-
+  const hasBio = false;
+  return (
+    <Modal style={{ width: "100%" }} onRequestClose={() => { !editVisible }}>
       <View>
-        <Image source={user1} style={editProfileStyle.profilePhoto}/>
-      </View>
+        {editVisible.editVisible ? (
+          <View style={editProfileStyle.container}>
+            <View style={editProfileStyle.top}>
+              <Text style={editProfileStyle.header}>Edit Profile</Text>
+            </View>
 
-      <View>
-        <Text>User Name</Text>
-        <Text>Name LastName</Text>
-      </View>
+            <View>
+              <TouchableOpacity>
+                <Image source={user1} style={editProfileStyle.profilePhoto} />
+              </TouchableOpacity>
+            </View>
 
-      <View>
-        {hasBio?(
-          <Slider/>
-        ):
-          <Text>You Don't have a biography</Text>
-        }
-      </View>
+            <View>
+              <Text>User Name</Text>
+              <TextInput
+                placeholder="Search"
+                style={editProfileStyle.searchBar}
+              />
+            </View>
+            <View>
+              <Text>Name LastName</Text>
+              <TextInput
+                placeholder="Search"
+                style={editProfileStyle.searchBar}
+              />
+            </View>
 
-    </View> 
-    ):null}
-    </View></Modal>
-    );  
-  }   
+            <View>
+              {hasBio ? (
+                <Slider />
+              ) :
+                <View>
+                  <Text>You Don't have a biography</Text>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Icon type="feather" size={"175%"} name={"mic"} />
+                    <Slider />
+                  </View>
+                </View>
+              }
+            </View>
+
+          </View>
+        ) : null}
+      </View></Modal>
+  );
+}   
