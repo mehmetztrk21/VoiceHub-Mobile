7d2447ec443ce17560aa5739caa845816f765821
Kaan Kayserili <knkayserili@gmail.com>
2023-02-16 14:11:38 +0300

    postText component design edit

--- VoiceHub-Mobile/assets/styles/postTexts.style.js
+++ VoiceHub-Mobile/assets/styles/postTexts.style.js
@@ -2,7 +2,6 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     textCounter:{
-        padding:5,
         marginHorizontal: "3%",
     },
     textHolder:{
@@ -11,9 +10,6 @@ export default StyleSheet.create({
     userCap:{
         fontWeight:"bold",
         marginRight:"1.5%",
-    },
-    otherComments:{
-        marginLeft:"1%",
     },
     UserComments:{
         flexDirection:"row",
