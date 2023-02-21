6e074f238a4e4f5d288c09a690b19f1e4629f134
Kaan Kayserili <knkayserili@gmail.com>
2023-02-21 19:02:31 +0300

    add voice modal's design

--- VoiceHub-Mobile/assets/styles/addVoice.style.js
+++ VoiceHub-Mobile/assets/styles/addVoice.style.js
@@ -1,33 +1,39 @@
 import { StyleSheet } from "react-native";
-
+import colors from "../colors"
 export default StyleSheet.create({
+  wrapper:{
+    position:"fixed",
+    width:'100%',
+    bottom:0,
+    zIndex:999,
+    backgroundColor:'#FFF'
+},
     container:{
         flex:1,
         width:'100%',
         flexDirection:"row",
         alignItems:"center",
-        justifyContent:"space-around",
+        justifyContent:"space-between",
         height:70,
         paddingVertical:10,
     },
-    wrapper:{
-        position:"fixed",
-        width:'100%',
-        bottom:0,
-        zIndex:999,
-        backgroundColor:'#FFF'
+    content:{
+      flexDirection:"row",
+      alignItems:"center",
+      paddingLeft:"5%",
     },
-      content: {
-        justifyContent: "center", 
-        flexDirection: "row", 
-        paddingLeft: '10%', 
-        paddingRight: '2.5%', 
-        width: "100%", 
-        alignItems: "center",
-      },
       time: {
         fontSize: 15,
-        marginRight: 10,
+        paddingLeft:"5%",
         fontWeight: "700",
       },
+      touch:{
+        marginRight:"5%",
+        padding:"1%",
+        borderRadius:"175%",
+        borderColor:"#212121",
+        borderWidth:2,
+        backgroundColor:colors.green,
+        color:colors.white,
+      }
 })
--- VoiceHub-Mobile/screens/components/addVoice.js
+++ VoiceHub-Mobile/screens/components/addVoice.js
@@ -1,32 +1,55 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
-import React, { useState } from 'react'
-import { Divider, Icon } from 'react-native-elements'
-import addVoice from '../../assets/styles/addVoice.style'
-import addVoiceStyle from '../../assets/styles/addVoice.style'
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Divider, Icon } from 'react-native-elements';
+import addVoiceStyle from '../../assets/styles/addVoice.style';
 import gif from "../../assets/images/rec.gif";
 import recImg from "../../assets/images/rec.png";
-let recordingTime=0;
+
 export default function AddVoice() {
-    const [record, setRecord] = useState(false)
+    const [seconds, setSeconds] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds + 1);
+        
+      }, 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const handleStartStop = () => {
+    setIsRunning(prevIsRunning => !prevIsRunning);
+  };
+
+  const handleReset = () => {
+    {/* go backend */}
+    setSeconds(0);
+    setIsRunning(false);
+  };
 
     return (
-        <View style={addVoice.wrapper}>
+        <View style={addVoiceStyle.wrapper}>
             <Divider width={1} orientation='vertical' />
 
-            <View style={addVoice.container}>
-                {record == true ? (
-                    <Image
-                        source={gif}
-                        style={{ width: 50, height: 50 }} />
-                ) :
-                    <Image
-                        source={recImg}
-                        style={{ width: 50, height: 50 }} />
-                }
-                <Text style={addVoiceStyle.time}>00:00</Text>
-                {console.log(recordingTime)}
-                <TouchableOpacity style={{ justifyContent: "center", alignContent: "center", bottom: 30 }}
-                    onPressIn={() => setRecord(true)} onPressOut={() => setRecord(false)}>
+            <View style={addVoiceStyle.container}>
+                <View style={addVoiceStyle.content}>
+                    {isRunning == true ? (
+                        <Image
+                            source={gif}
+                            style={{ width: 50, height: 50, borderRadius:25 }} />
+                    ) :
+                        <Image
+                            source={recImg}
+                            style={{ width: 50, height: 50, borderRadius:25 }} />
+                    }
+                    <Text style={addVoiceStyle.time}>{(Math.floor(seconds/60))+":"+(seconds%60)}</Text>
+                </View>
+
+                <TouchableOpacity style={addVoiceStyle.touch}
+                    onPressIn={handleStartStop} onPressOut={handleReset}>
                     <Icon type="feather" size={"175%"} name={"mic"} />
                 </TouchableOpacity>
             </View>
