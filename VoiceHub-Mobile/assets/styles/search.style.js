dccc6df5a7ca96da941bacb48c675d3afc8f4617
Kaan Kayserili <knkayserili@gmail.com>
2023-02-21 14:57:13 +0300

    edit search textInputs open last searched mechanic

--- VoiceHub-Mobile/assets/styles/search.style.js
+++ VoiceHub-Mobile/assets/styles/search.style.js
@@ -10,10 +10,8 @@ export default StyleSheet.create({
 
   searchBarHolder: {
     marginTop:60,
-    left: 0,
     width: "100%",
     height: 80,
-    alignItems: "center",
     justifyContent: "center",
   },
 
@@ -22,22 +20,17 @@ export default StyleSheet.create({
     borderRadius: 15,
     paddingVertical:10,
     paddingHorizontal:12.5,
-    width: "90%",
   },
-
+  widthChange2:{
+    width:"87.5%",
+    marginLeft:"6.25%"
+  },
+  widthChange1:{
+    width:"80%",
+    marginLeft:"6.25%"
+  },
   scrollContainer: {
     backgroundColor: "white",
     width: "95%",
-  },
-  
-  searchImg:{
-    width: 50, 
-    height: 50,
-    borderRadius:25, 
-    marginLeft:"10%", 
-    marginRight:"5%",
-  },
-  slider:{
-    width:"60%",
   },
 });
--- VoiceHub-Mobile/screens/search.js
+++ VoiceHub-Mobile/screens/search.js
@@ -1,15 +1,16 @@
 import React, { useRef, useState } from "react";
-import { ScrollView, TouchableOpacity, View, Text, Image, TextInput, SafeAreaView } from "react-native";
+import { ScrollView, TouchableOpacity, View, TextInput, SafeAreaView } from "react-native";
 
 import RenderDiscover from "./components/RenderDiscover";
 import RenderLastSearchedUser from "./components/RenderLastSearchedUser";
 import BottomTabs from "./components/BottomTabs";
 import SearchHeader from "./components/SearchHeader";
 import searchStyles from '../assets/styles/search.style';
+import { Icon } from "react-native-elements";
 
 export default function SearchScreen({ navigation, route }) {
   const { userName } = route.params;
-  const [focused, setFocused] = useState(true);
+  const [focused, setFocused] = useState(false);
   {/*Coming Soon --->*/ }
   const [searchQuery, setSearchQuery] = useState("");
   const onChangeSearch = (query) => setSearchQuery(query);
@@ -19,22 +20,37 @@ export default function SearchScreen({ navigation, route }) {
 
   const handleScrollToTop = () => {
     console.log('yukarı kaydı')
-    scrollViewRef.current.scrollTo({y:0})
+    scrollViewRef.current.scrollTo({ y: 0 })
   };
 
   return (
     <SafeAreaView style={searchStyles.container}>
 
-    <SearchHeader pressLogo={handleScrollToTop}/>
+      <SearchHeader pressLogo={handleScrollToTop} />
 
       <View style={searchStyles.searchBarHolder}>
-        <TextInput
-          placeholder="Search"
-          style={searchStyles.searchBar}
-          onChangeText={onChangeSearch}
-          value={searchQuery}
-          onFocus={() => setFocused(!focused)}
-        />
+        {focused==true ? (
+          <View style={{flexDirection:"row"}}>
+            <TextInput
+              placeholder="Search"
+              style={[searchStyles.searchBar,searchStyles.widthChange1]}
+              onChangeText={onChangeSearch}
+              value={searchQuery}
+            />
+            <TouchableOpacity onPress={() => {setFocused(false); setSearchQuery("")}}>
+              <Icon type="font-awesome" size={"175%"} name={"times"} style={searchStyles.closeButton} />
+            </TouchableOpacity>
+          </View>
+
+        ) :
+          <TextInput
+            placeholder="Search"
+            style={[searchStyles.searchBar,searchStyles.widthChange2]}
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+            onFocus={() => setFocused(true)}
+          />
+        }
       </View>
 
       <ScrollView
@@ -42,14 +58,14 @@ export default function SearchScreen({ navigation, route }) {
         style={searchStyles.scrollContainer}
         ref={scrollViewRef}
       >
-        {focused ? (
+        {focused==false ? (
           <View style={searchStyles.userHodler}>
             {/* Get Users Infos */}
-            <RenderDiscover  navigation={navigation}/>
+            <RenderDiscover navigation={navigation} />
           </View>
         ) :
           <View style={searchStyles.userHodler}>
-            <RenderLastSearchedUser navigation={navigation}/>
+            <RenderLastSearchedUser navigation={navigation} />
           </View>
         }
       </ScrollView>
