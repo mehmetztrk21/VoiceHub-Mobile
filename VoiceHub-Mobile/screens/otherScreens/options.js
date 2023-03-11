1f0d82f44ac33677a9bea0d01f9a18bbc7f93f34
Kaan Kayserili <knkayserili@gmail.com>
2023-03-11 18:40:06 +0300

    edited options page's background

--- VoiceHub-Mobile/screens/otherScreens/options.js
+++ VoiceHub-Mobile/screens/otherScreens/options.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import colors from '../../assets/colors';
 import OtherHeader from "../components/otherHeader";
 
 const options = ({ navigation }) => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{backgroundColor:colors.white, flex:1, width:'100%'}}>
             <OtherHeader navigation={navigation} HeaderTitle={'Options'}/>
             <Text>Options Page is Here!</Text>
         </SafeAreaView>
