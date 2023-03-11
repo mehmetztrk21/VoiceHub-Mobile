6b7d81b5c380a0399a63e1c2899aa4d3868d59a9
Kaan Kayserili <knkayserili@gmail.com>
2023-03-11 18:37:45 +0300

    options page added otherHeader component

--- VoiceHub-Mobile/screens/otherScreens/options.js
+++ VoiceHub-Mobile/screens/otherScreens/options.js
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import OtherHeader from "../components/otherHeader";
 
-const options = ({navigation}) => {
-  return (
-    <View>
-      <Text>Options Page is Here!</Text>
-    </View>
-  )
+const options = ({ navigation }) => {
+    return (
+        <SafeAreaView>
+            <OtherHeader navigation={navigation} HeaderTitle={'Options'}/>
+            <Text>Options Page is Here!</Text>
+        </SafeAreaView>
+    )
 }
 
 export default options
