4ad2a368fedc327f461e31bc5cf586394c338b83
Kaan Kayserili <knkayserili@gmail.com>
2023-03-11 18:24:34 +0300

    options page is created but it is empty page

---
+++ VoiceHub-Mobile/screens/otherScreens/options.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const options = ({navigation}) => {
+  return (
+    <View>
+      <Text>Options Page is Here!</Text>
+    </View>
+  )
+}
+
+export default options
