572225371f6c34195f627d281d745e239b4b6390
Kaan Kayserili <knkayserili@gmail.com>
2023-03-13 13:09:00 +0300

    edit addVoice component's design

--- VoiceHub-Mobile/assets/styles/addVoice.style.js
+++ VoiceHub-Mobile/assets/styles/addVoice.style.js
@@ -1,39 +1,43 @@
 import { StyleSheet } from "react-native";
-import colors from "../colors"
+import colors from "../colors";
+
 export default StyleSheet.create({
-  wrapper:{
-    position:"fixed",
-    width:'100%',
-    zIndex:999,
-    backgroundColor:'#FFF',
-    bottom:0,
-},
-    container:{
-        flex:1,
-        width:'100%',
-        flexDirection:"row",
-        alignItems:"center",
-        justifyContent:"space-between",
-        height:70,
-        paddingVertical:10,
-    },
-    content:{
-      flexDirection:"row",
-      alignItems:"center",
-      paddingLeft:"5%",
-    },
-      time: {
-        fontSize: 15,
-        paddingLeft:"5%",
-        fontWeight: "700",
-      },
-      touch:{
-        marginRight:"5%",
-        padding:"1%",
-        borderRadius:"175%",
-        borderColor:"#212121",
-        borderWidth:2,
-        backgroundColor:colors.green,
-        color:colors.white,
-      }
+  wrapper: {
+    position: "fixed",
+    width: '100%',
+    zIndex: 999,
+    backgroundColor: '#FFF',
+    bottom: 0,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  container: {
+    flex: 1,
+    width: '100%',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 70,
+    paddingVertical: 10,
+
+  },
+  content: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: "5%",
+  },
+  time: {
+    fontSize: 15,
+    paddingLeft: "5%",
+    fontWeight: "700",
+  },
+  touch: {
+    marginRight: "5%",
+    padding: "1%",
+    borderRadius: "175%",
+    borderColor: "#212121",
+    borderWidth: 2,
+    backgroundColor: colors.green,
+    color: colors.white,
+  }
 })
