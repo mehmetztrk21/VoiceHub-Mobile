4e1a4e5b38efde980997f0edd3e69a6fdb7b23b6
Kaan Kayserili <knkayserili@gmail.com>
2023-03-13 11:57:27 +0300

    add delete last searched user component'S button

--- VoiceHub-Mobile/assets/styles/RenderLastSearchedUser.style.js
+++ VoiceHub-Mobile/assets/styles/RenderLastSearchedUser.style.js
@@ -5,11 +5,16 @@ export default StyleSheet.create({
         paddingBottom: 20,
         width:"90%",
         marginHorizontal:"5%",
+        flexDirection:"row",
       },
       lastSearchImage:{
         width: 50, 
         height: 50, 
         borderRadius:25, 
         marginRight:"1%",
+      },
+      closeButtonTouch:{
+        justifyContent:"center",
+        marginLeft: 16,
       },
 })
