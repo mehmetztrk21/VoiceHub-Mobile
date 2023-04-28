53044d1c21a7e2fa8a7df28a0c0fbef4d1e40ac5
Kaan Kayserili <knkayserili@gmail.com>
2023-04-28 12:29:12 +0300

    create change password page and connection

---
+++ VoiceHub-Mobile/screens/otherScreens/changePassword.js
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-elements';
+import OtherHeader from "../../screens/components/otherHeader";
+import { changePassword } from '../../services/userServices';
+import { useUser } from '../../utils/userContext';
+import colors from '../../assets/colors';
+
+const ChangePassword = ({ navigation }) => {
+    const [old, setOld] = useState("");
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+    const confirm = async () => {
+
+        if (password1 == password2) {
+            await changePassword({ password: old, newPassword: password2 }).then(async (res) => {
+                if (res?.success) {
+                    alert("Şifreniz Başarı ile değiştirildi")
+                }
+                else {
+                    alert("Eski şifrenizi doğru girmediniz")
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+        else {
+            alert("Yeni oluşturacağınız şifreler birbiriyle uyuşmuyor")
+        }
+
+    };
+
+    const handlePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    };
+
+    return (
+        <SafeAreaView style={{ backgroundColor: colors.white, flex: 1, width: "100%" }}>
+            <OtherHeader navigation={navigation} HeaderTitle={'Change Password'} isTic={false} />
+            <View style={{ marginTop: "25%" }}>
+                <Text style={{
+                    marginBottom: "0.5%",
+                    marginTop: "1.5%",
+                    marginLeft: "8%",
+                    fontWeight: "500",
+                    fontSize: 16,
+                }}>Old Password</Text>
+                <View style={{
+                    backgroundColor: "lightgray",
+                    borderRadius: 25,
+                    paddingVertical: 10,
+                    paddingHorizontal: "2%",
+                    width: "80%",
+                    marginLeft: "8%",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                }}>
+                    <TextInput
+                        style={{ width: "80%" }}
+                        maxLength={18}
+                        value={old}
+                        secureTextEntry={!isPasswordVisible}
+                        onChangeText={old => setOld(old)}
+                    />
+                    <TouchableOpacity onPress={handlePasswordVisibility}>
+                        <Icon type="font-awesome" size={20} name={isPasswordVisible ? "eye" : "eye-slash"} color={colors.green} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={{ marginTop: "5%" }}>
+                <Text style={{
+                    marginBottom: "0.5%",
+                    marginTop: "1.5%",
+                    marginLeft: "8%",
+                    fontWeight: "500",
+                    fontSize: 16,
+                }}>New Password</Text>
+                <TextInput
+                    style={{
+                        backgroundColor: "lightgray",
+                        borderRadius: 25,
+                        paddingVertical: 10,
+                        paddingHorizontal: "2%",
+                        width: "80%",
+                        marginLeft: "8%",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                    }}
+                    maxLength={18}
+                    value={password1}
+                    secureTextEntry={!isPasswordVisible}
+                    onChangeText={password1 => setPassword1(password1)}
+                />
+            </View>
+
+            <View style={{ marginTop: "5%" }}>
+                <Text style={{
+                    marginBottom: "0.5%",
+                    marginTop: "1.5%",
+                    marginLeft: "8%",
+                    fontWeight: "500",
+                    fontSize: 16,
+                }}> New Password Repeat</Text>
+                <TextInput
+                    style={{
+                        backgroundColor: "lightgray",
+                        borderRadius: 25,
+                        paddingVertical: 10,
+                        paddingHorizontal: "2%",
+                        width: "80%",
+                        marginLeft: "8%",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                    }}
+                    maxLength={18}
+                    value={password2}
+                    secureTextEntry={!isPasswordVisible}
+                    onChangeText={password2 => setPassword2(password2)}
+                />
+            </View>
+
+            <TouchableOpacity onPress={confirm}
+                style={{ backgroundColor: colors.green, paddingHorizontal: 20, paddingVertical: 10, borderRadius: 40, width: "50%", marginLeft: "25%", marginTop: "5%" }}>
+                <Text style={{ fontSize: 16, color: colors.white, fontWeight: "600" }}> Confirm</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
+}
+
+export default ChangePassword
--- VoiceHub-Mobile/screens/otherScreens/options.js
+++ VoiceHub-Mobile/screens/otherScreens/options.js
@@ -1,24 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, Switch, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Switch, Text, View } from 'react-native';
 import colors from '../../assets/colors';
 import OtherHeader from "../components/otherHeader";
 
-import { Dimensions } from "react-native";
+import { Dimensions, TouchableOpacity } from "react-native";
 import optionsStyle from '../../assets/styles/options.style';
-import { TouchableOpacity } from 'react-native';
-import { Icon } from 'react-native-elements';
-import { changePassword, updateUserInfo } from '../../services/userServices';
-import { useUser } from "../../utils/userContext"
+import { updateUserInfo } from '../../services/userServices';
+import { useUser } from "../../utils/userContext";
 const { width } = Dimensions.get("window");
 
 const Options = ({ navigation }) => {
 
     const { user } = useUser();
-    const [old, setOld] = useState("");
-    const [password1, setPassword1] = useState("");
-    const [password2, setPassword2] = useState("");
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-
     const [isSecretAccount, setIsSecretAccount] = useState(user?.isSecretAccount);
 
     const toggleSwitch = async () => {
@@ -29,35 +22,11 @@ const Options = ({ navigation }) => {
 
     }
 
-    const confirm = async () => {
-
-        if (password1 == password2) {
-            await changePassword({ password: old, newPassword: password2 }).then(async (res) => {
-                if (res?.success) {
-                    alert("Şifreniz Başarı ile değiştirildi")
-                }
-                else {
-                    alert("Eski şifrenizi doğru girmediniz")
-                }
-            }).catch((err) => {
-                console.log(err);
-            })
-        }
-        else {
-            alert("Yeni oluşturacağınız şifreler birbiriyle uyuşmuyor")
-        }
-
-    };
-
-    const handlePasswordVisibility = () => {
-        setIsPasswordVisible(!isPasswordVisible);
-    };
-
     return (
         <SafeAreaView style={{ backgroundColor: colors.white, flex: 1, width: '100%' }}>
             <OtherHeader navigation={navigation} HeaderTitle={'Options'} isTic={false} />
 
-            <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: 'center', marginTop: "30%", marginRight: "10%" }}>
+            <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: 'center', marginTop: "25%", marginRight: "10%" }}>
                 <Text style={optionsStyle.label}>Secret Account</Text>
 
                 <Switch
@@ -69,12 +38,13 @@ const Options = ({ navigation }) => {
                 />
             </View>
 
-            <TouchableOpacity>
-                <Text>Change Password</Text>
+            <TouchableOpacity onPress={() => { navigation.navigate("ChangePassword") }}
+                style={{ backgroundColor: colors.green, paddingHorizontal: 20, paddingVertical: 10, borderRadius: 40, width: "50%", marginLeft: "25%", marginTop: 20 }}>
+                <Text style={{ fontSize: 16, color: colors.white, fontWeight: "600" }}> Change Password</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={()=>{navigation.navigate("Blockeds")}}
-                style={{ backgroundColor: colors.green, paddingHorizontal: 20, paddingVertical: 10, borderRadius: 40, width: "50%", marginLeft: "25%" }}>
+            <TouchableOpacity onPress={() => { navigation.navigate("Blockeds") }}
+                style={{ backgroundColor: colors.green, paddingHorizontal: 20, paddingVertical: 10, borderRadius: 40, width: "50%", marginLeft: "25%", marginTop: 20 }}>
                 <Text style={{ fontSize: 16, color: colors.white, fontWeight: "600" }}> Blocked Accounts</Text>
             </TouchableOpacity>
 
