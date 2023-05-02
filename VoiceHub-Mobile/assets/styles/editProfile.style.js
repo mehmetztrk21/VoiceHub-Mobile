1d277f96051384a4390d309bb76c8a541e722825
Kaan Kayserili <knkayserili@gmail.com>
2023-05-03 00:42:27 +0300

    add picker to editProfile

--- VoiceHub-Mobile/assets/styles/editProfile.style.js
+++ VoiceHub-Mobile/assets/styles/editProfile.style.js
@@ -1,50 +1,55 @@
 import { StyleSheet } from "react-native";
 import colors from "../colors"
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        width:"100%",
-        backgroundColor: colors.white,
-      },
-      searchBar: {
-        backgroundColor: colors.lightgray,
-        borderRadius: 15,
-        paddingVertical:"2.5%",
-        paddingHorizontal:"2.5%",
-        width: "80%",
-        marginHorizontal:"10%"
-      },
-      ppView:{
-        flexDirection:"column",
-        alignItems:"center",
-        marginTop:70,
-        marginBottom:"2%",
-        paddingBottom:"2%"
-      },
-      profilePhoto:{
-        width:140,
-        height:140,
-        borderRadius:70,
-      },
-      editPhotoText:{
-        color:colors.green,
-        fontSize:16,
-        fontWeight:"700"
-      },
-      saveButtonText:{
-        color:colors.white,
-        fontSize:26,
-        fontWeight:"700",
-        width:"50%",
-        padding:5,
-        marginLeft:"50%",
-        borderRadius:26,
-        marginBottom:"20%"
-      },
-      TextView:{
-        marginTop:"2%"
-      },
-      label:{
-        marginHorizontal:"10%",
-      }
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: colors.white,
+  },
+  searchBar: {
+    backgroundColor: colors.lightgray,
+    borderRadius: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    width: "80%",
+    marginHorizontal: "10%",
+  },
+  ppView: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 70,
+    marginBottom: "2%",
+    paddingBottom: "2%",
+  },
+  profilePhoto: {
+    width: 140,
+    height: 140,
+    borderRadius: 70,
+  },
+  editPhotoText: {
+    color: colors.green,
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  saveButtonText: {
+    color: colors.white,
+    fontSize: 26,
+    fontWeight: "700",
+    width: "50%",
+    padding: 5,
+    marginLeft: "25%",
+    borderRadius: 26,
+    marginBottom: "20%",
+    backgroundColor: colors.green,
+    textAlign: "center",
+  },
+  TextView: {
+    marginTop: "2%",
+  },
+  label: {
+    marginTop: "3%",
+    marginBottom: "1%",
+    marginHorizontal: "10%",
+    fontWeight:"500",
+  }
 });
--- VoiceHub-Mobile/screens/otherScreens/editProfile.js
+++ VoiceHub-Mobile/screens/otherScreens/editProfile.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Image, Modal, SafeAreaView, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { Icon } from "react-native-elements";
 
-import DocumentPicker from "react-native-document-picker";
 
 import colors from "../../assets/colors";
 import editProfileStyle from "../../assets/styles/editProfile.style";
 
 import AddVoice from "../components/addVoice";
-import OtherHeader from "../components/otherHeader";
 import BioVoicePopUp from "../components/bioVoicePopUp";
+import OtherHeader from "../components/otherHeader";
 import Slider from "../components/slider";
 
 import { Dimensions } from "react-native";
+import { getUserById, updateUserInfo } from "../../services/userServices";
 import { baseURL } from "../../utils/constants";
-import { getUserById, updateUserInfo } from "../../services/userServices"
-import ProfilePhotoPopUp from "../components/profilePhotoPopUp";
 import { useUser } from "../../utils/userContext";
+import ProfilePhotoPopUp from "../components/profilePhotoPopUp";
+import { Picker } from "@react-native-picker/picker";
 const { width } = Dimensions.get("window");
 
 export default function EditProfile({ navigation }) {
@@ -62,109 +62,121 @@ export default function EditProfile({ navigation }) {
     <SafeAreaView style={editProfileStyle.container}>
       <OtherHeader HeaderTitle="Edit Profile" navigation={navigation} isTic={false} />
 
-      <ScrollView>
-        <Modal visible={openProfilePhotoPopUp}
-          animationType="slide"
-          transparent={true}
-          onRequestClose={() => {
-            setOpenProfilePhotoPopUp(false)
-          }}>
-          <ProfilePhotoPopUp setOpenProfilePhotoPopUp={setOpenProfilePhotoPopUp} />
-        </Modal>
+      <Modal visible={openProfilePhotoPopUp}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => {
+          setOpenProfilePhotoPopUp(false)
+        }}>
+        <ProfilePhotoPopUp setOpenProfilePhotoPopUp={setOpenProfilePhotoPopUp} />
+      </Modal>
 
-        <Modal visible={openBioVoicePopUp}
-          animationType="slide"
-          transparent={true}
-          onRequestClose={() => {
-            setOpenBioVoicePopUp(false)
-          }}>
-          <BioVoicePopUp setOpenAddVoice={setOpenAddVoice} setOpenBioVoicePopUp={setOpenBioVoicePopUp} />
-        </Modal>
+      <Modal visible={openBioVoicePopUp}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => {
+          setOpenBioVoicePopUp(false)
+        }}>
+        <BioVoicePopUp setOpenAddVoice={setOpenAddVoice} setOpenBioVoicePopUp={setOpenBioVoicePopUp} />
+      </Modal>
 
-        <View style={{ flexDirection: "column", marginTop: width * 0.07 }}>
-          <View>
-            <TouchableOpacity style={editProfileStyle.ppView} onPress={() => { setOpenProfilePhotoPopUp(true) }}>
-              {user?.profilePhotoUrl ?
-                <Image source={{ uri: baseURL + user?.profilePhotoUrl }} style={editProfileStyle.profilePhoto} /> :
-                <Image source={require('../../assets/avatar.png')} style={editProfileStyle.profilePhoto} />
-              }
-              <Text style={editProfileStyle.editPhotoText}>Edit Profile Photo</Text>
-            </TouchableOpacity>
-          </View>
-
-          <Text style={editProfileStyle.label}>User Name</Text>
-          <TextInput
-            value={username}
-            onChangeText={(username) => setUserName(username)}
-            style={editProfileStyle.searchBar}
-          />
-
-          <Text style={editProfileStyle.label}>Name</Text>
-          <TextInput
-            value={firstname}
-            onChangeText={firstname => setFirstName(firstname)}
-            style={editProfileStyle.searchBar}
-          />
-
-          <Text style={editProfileStyle.label}>Surname</Text>
-          <TextInput
-            value={surname}
-            onChangeText={surname => setSurname(surname)}
-            style={editProfileStyle.searchBar}
-          />
-
-          <Text style={editProfileStyle.label}>Phone</Text>
-          <TextInput
-            value={phone}
-            onChangeText={phone => setPhone(phone)}
-            style={editProfileStyle.searchBar}
-          />
-
-          <Text style={editProfileStyle.label}>Birth Day</Text>
-          <TextInput
-            value={birthDay}
-            onChangeText={birthDay => setBirthDay(birthDay)}
-            style={editProfileStyle.searchBar}
-          />
-
-          <Text style={editProfileStyle.label}>Gender</Text>
-          <TextInput
-            value={gender}
-            onChangeText={(gender) => setGender(gender)}
-            style={editProfileStyle.searchBar}
-          />
-
-
-          <View style={{ marginVertical: "3%", marginHorizontal: "10%" }}>
-            {user?.descriptionVoiceUrl != null ? (
-              <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
-                <Icon type="feather" size={28} name={"play"} color={colors.black} style={{ paddingRight: 10 }} />
-                <Slider />
-                <TouchableOpacity onPress={() => { setOpenBioVoicePopUp(true) }}>
-                  <Text style={{ color: colors.green, fontSize: 14, fontWeight: "700", paddingLeft: 10 }}>Edit</Text>
-                </TouchableOpacity>
-              </View>
-            ) :
-              <View style={{ justifyContent: "center" }}>
-                <Text style={{ color: colors.darkGray, fontSize: 14, fontWeight: "400", textAlign: "center", marginTop: "5%", marginBottom: "5%" }}>
-                  {"You Don't have a biography"}</Text>
-
-                <TouchableOpacity onPress={() => setOpenAddVoice(prev => !prev)}
-                  style={{ width: "50%", marginLeft: "25%", backgroundColor: colors.green, borderRadius: 50, padding: 5, }}>
-                  <Text style={{ color: colors.white, fontSize: 14, fontWeight: "700", textAlign: "center", }}>Add Voice</Text>
-                </TouchableOpacity>
-              </View>
+      <ScrollView style={{ flexDirection: "column", marginTop: width * 0.07 }}>
+        <View>
+          <TouchableOpacity style={editProfileStyle.ppView} onPress={() => { setOpenProfilePhotoPopUp(true) }}>
+            {user?.profilePhotoUrl ?
+              <Image source={{ uri: baseURL + user?.profilePhotoUrl }} style={editProfileStyle.profilePhoto} /> :
+              <Image source={require('../../assets/avatar.png')} style={editProfileStyle.profilePhoto} />
             }
-          </View>
+            <Text style={editProfileStyle.editPhotoText}>Edit Profile Photo</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text style={editProfileStyle.label}>User Name</Text>
+        <TextInput
+          value={username}
+          onChangeText={(username) => setUserName(username)}
+          style={editProfileStyle.searchBar}
+        />
+
+        <Text style={editProfileStyle.label}>Name</Text>
+        <TextInput
+          value={firstname}
+          onChangeText={firstname => setFirstName(firstname)}
+          style={editProfileStyle.searchBar}
+        />
+
+        <Text style={editProfileStyle.label}>Surname</Text>
+        <TextInput
+          value={surname}
+          onChangeText={surname => setSurname(surname)}
+          style={editProfileStyle.searchBar}
+        />
+
+        <Text style={editProfileStyle.label}>Phone</Text>
+        <TextInput
+          value={phone}
+          onChangeText={phone => setPhone(phone)}
+          style={editProfileStyle.searchBar}
+        />
+
+        <Text style={editProfileStyle.label}>Birth Day</Text>
+        <DatePicker
+        modal
+        open={open}
+        date={date}
+        onConfirm={(date) => {
+          setOpen(false)
+          setDate(date)
+        }}
+        onCancel={() => {
+          setOpen(false)
+        }}
+      />
+
+        <Text style={editProfileStyle.label}>Gender</Text>
+        <Picker
+          style={{
+            backgroundColor: colors.lightgray,
+            borderRadius: 15,
+            paddingVertical: "2.5%",
+            paddingHorizontal: "2.5%",
+            width: "80%",
+            marginHorizontal: "10%",
+          }}
+          value={gender}
+          selectedValue={gender}
+          onValueChange={(gender) => setGender(gender)}>
+          <Picker.Item label="Choose Gender" value="" />
+          <Picker.Item label={"Male"} value={"male"} />
+          <Picker.Item label={"Female"} value={"female"} />
+        </Picker>
 
 
+        <View style={{ marginVertical: "3%", marginHorizontal: "10%" }}>
+          {user?.descriptionVoiceUrl != null ? (
+            <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+              <Icon type="feather" size={28} name={"play"} color={colors.black} style={{ paddingRight: 10 }} />
+              <Slider />
+              <TouchableOpacity onPress={() => { setOpenBioVoicePopUp(true) }}>
+                <Text style={{ color: colors.green, fontSize: 14, fontWeight: "700", paddingLeft: 10 }}>Edit</Text>
+              </TouchableOpacity>
+            </View>
+          ) :
+            <View style={{ justifyContent: "center" }}>
+              <Text style={{ color: colors.darkGray, fontSize: 14, fontWeight: "500", textAlign: "center", marginTop: "5%", marginBottom: "5%" }}>
+                {"You Don't have a biography"}</Text>
 
-
+              <TouchableOpacity onPress={() => setOpenAddVoice(prev => !prev)}
+                style={{ width: "50%", marginLeft: "25%", backgroundColor: colors.green, borderRadius: 50, padding: 5, }}>
+                <Text style={{ color: colors.white, fontSize: 14, fontWeight: "700", textAlign: "center", }}>Add Voice</Text>
+              </TouchableOpacity>
+            </View>
+          }
         </View>
 
         {!((firstname == "") && (surname == "") && (username == "") && (phone == "") && (birthDay == "") && (gender == "")) ?
-          <TouchableOpacity onPress={save} style={{ position: "absolute", bottom: 0, zIndex: 999 }}>
-            <Text style={[editProfileStyle.saveButtonText, { backgroundColor: colors.green, textAlign: "center" }]}>Save</Text>
+          <TouchableOpacity onPress={save}>
+            <Text style={editProfileStyle.saveButtonText}>Save</Text>
           </TouchableOpacity>
           : null}
 
