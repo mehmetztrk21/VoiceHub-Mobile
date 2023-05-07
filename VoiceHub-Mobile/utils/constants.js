0bb8daa6bdc2d3c7884fe794d1031540ed6423f3
Kaan Kayserili <knkayserili@gmail.com>
2023-05-07 15:29:35 +0300

    delete constants

--- VoiceHub-Mobile/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const baseURL = "http://10.0.2.2:5000/"
-
-//kaan http://10.0.2.2:5000/
-//z.burnu kütüphane http://192.168.27.84:5000/
-//mehmet http://192.168.0.33:5000/
-//telefon http://192.168.1.6:5000/
